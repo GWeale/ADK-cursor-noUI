d9edb41602b6354bfda95a7fc9225487ef942da3
ADK Coding Agent Project <dev@adk-coding-agent.local>
2025-07-16 14:50:57 -0700

    added testa and tools to try and also change the files, i think the rile reader is not gettingt he correct scope as it can currently only add and move files around in the agent folder

---
+++ example_code.py
@@ -0,0 +1,10 @@
+def hello_world():
+    print("Hello, World!")
+
+def add_numbers(a, b):
+    return a + b
+
+def calculate_area(length, width):
+    return length * width
+
+# TODO: Add a function to calculate the perimeter of a rectangle 
